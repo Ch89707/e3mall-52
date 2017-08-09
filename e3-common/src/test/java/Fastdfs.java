19262c16372919e69ed0c827b0e68aa085b6d518
陈航 <897076409@qq.com>
2017-08-09 16:36:32 +0800

    sdf

---
+++ e3-common/src/test/java/Fastdfs.java
@@ -0,0 +1,12 @@
+
+import org.csource.fastdfs.ClientGlobal;
+import org.junit.Test;
+
+public class Fastdfs {
+
+	@Test
+	public void testUpLoadFile() throws Exception{
+		ClientGlobal.init("D:/workspaces-itcast/e3-manager-web/src/main/resources/resource/client.conf");
+	}
+}
+
